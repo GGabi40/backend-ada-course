5df2c8d214df311f5570cd1c76b82ef14fd23da2
ggabi40 <gabibc2000@hotmail.com>
2024-12-19 18:23:46 -0300

    prompt con Async Await

--- NODE-I/mod-io.js
+++ NODE-I/mod-io.js
@@ -9,9 +9,64 @@ const prompt = rl.createInterface({
     output: process.stdout // devuelve por terminal
 })
 
+const promptPromise = {
+    // se dice que recibe un parámetro (pregunta), 
+    // haciendo con que se repase la pregunta como parámetro
+    question: (pregunta) => new Promise((resolve, reject) => {
+        try {
+            prompt.question(pregunta, (respuesta) =>  resolve(respuesta))
+        } catch(e) {
+            reject(e);
+        }
+    }),
+    close: () => prompt.close()
+}
+
 // Arranco el prompt con .question:
-// .question recibe: (string, callback Function)
+// prompt.question recibe: (string, callback Function)
+
+
+// Funciona, pero si tengo muchas preguntas, me queda
+// como un árbol:
+// Para evitarlo, se puede hacer el promptPromise
+
+/* 
+SIN promptPromise
+
 prompt.question('Cuál es tu numero favorito? ', (respuesta) => {
     console.log(`El doble de tu numero favorito es: ${parseInt(respuesta) * 2}`);
-    prompt.close(); // cierra la consola
+
+    prompt.question('Cuál es tu color favorito? ', (respuesta) => {
+        console.log(`Tu color favorito es: ${respuesta}`);
+    
+        prompt.close(); // cierra la consola
+    });
 });
+ */
+
+
+/*  CON promptPromise: */
+
+    // con async | await:
+async function askUser() {
+    const numero = await promptPromise.question('Cuál es tu numero favorito? ');
+    console.log(`El doble de tu numero favorito es: ${parseInt(numero) * 2}`);
+
+    // agregando otra pregunta:
+    const color = await promptPromise.question('Cuál es tu color favorito? ');
+    console.log(`Tu color favorito es: ${color}`);
+
+    promptPromise.close();
+}
+
+askUser();
+
+/* promptPromise.question('Cuál es tu numero favorito? ', (respuesta) => {
+    console.log(`El doble de tu numero favorito es: ${parseInt(respuesta) * 2}`);
+
+    prompt.question('Cuál es tu color favorito? ', (respuesta) => {
+        console.log(`Tu color favorito es: ${respuesta}`);
+    
+        prompt.close(); // cierra la consola
+    });
+}); */
